59fcd08e9ada835d0b0ffdd2bd9302e0937492a4
tgenchik <gteplakov5@gmail.com>
2025-02-17 19:11:39 +0300

    Create LongNumber.hpp

---
+++ LongNumber.hpp
@@ -0,0 +1,40 @@
+#ifndef LONGNUMBER_HPP
+#define LONGNUMBER_HPP
+
+#include <vector>
+#include <string>
+
+class LongNumber {
+public:
+    LongNumber();
+    LongNumber(long double number);
+    LongNumber(const LongNumber& other);
+    ~LongNumber();
+
+    LongNumber& operator=(const LongNumber& other);
+    LongNumber operator+(const LongNumber& other) const;
+    LongNumber operator-(const LongNumber& other) const;
+    LongNumber operator*(const LongNumber& other) const;
+    LongNumber operator/(const LongNumber& other) const;
+
+    bool operator==(const LongNumber& other) const;
+    bool operator!=(const LongNumber& other) const;
+    bool operator<(const LongNumber& other) const;
+    bool operator>(const LongNumber& other) const;
+
+    LongNumber operator-() const;
+
+    void setPrecision(int binaryDigits);
+
+    std::string toString() const;
+
+    std::vector<int> digits;
+    int precision;          
+    bool isNegative;         
+
+   
+    void deleteZeros();
+    void addDigit(int digit);
+};
+LongNumber operator ""_longnum(long double);
+#endif 
