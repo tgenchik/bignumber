f5c6150b133c8f4afd68faac3ca385999b603196
tgenchik <gteplakov5@gmail.com>
2025-02-18 14:04:05 +0300

    Update Longnumber.cpp

--- Longnumber.cpp
+++ Longnumber.cpp
@@ -1,6 +1,7 @@
 #include "LongNumber.hpp"
 #include <algorithm>
 #include <cmath>
+#include <string>
 #include <iostream>
 
 LongNumber::LongNumber() : precision(0), isNegative(false) {}
@@ -90,52 +91,51 @@ LongNumber LongNumber::operator+(const LongNumber& other) const {
     int max_precision = max(precision, other.precision);
     int max_digit = max(digits.size() - precision, other.digits.size() - other.precision);
     int diff = precision - other.precision;
-
     result.digits.resize(max_precision + max_digit + 1);
     result.precision = max_precision;
     int extra_digit = 0;
-        for (int i = 0; i < max_precision; i++) {
-            int now = extra_digit;
-            extra_digit = 0;
-            if (precision >= i)
-                if (diff >= 0)
-                    now += digits[digits.size() - i - 1];
-                else
-                    now += digits[digits.size() - i - 1 - diff];
-
-            if (other.precision >= i)
-                if (diff <= 0)
-                    now += other.digits[other.digits.size() - i - 1];
-                else
-                    now += other.digits[other.digits.size() - i - 1 + diff];
+    std::cout << diff <<"\n";
+    int last_1 = digits.size() - 1;
+    int last_2 = other.digits.size() - 1;
+    for (int i = 0; i < max_precision; i++) {
+        int now = extra_digit;
+        extra_digit = 0;
+        
+        if (max_precision - precision <= i)
+            now += digits[last_1--];
             
-            if (now > 1) {
-                extra_digit = 1;
-                now -= 2;
-            }
-            result.digits[result.digits.size() - 1 - i] = now;
+        
+        if (max_precision - other.precision <= i)
+            now += other.digits[last_2--];
+        
+        
+        if (now > 1) {
+            extra_digit = 1;
+            now -= 2;
         }
+        
+        result.digits[result.digits.size() - 1 - i] = now;
+    }
+    for (int i = 0; i < max_digit; i++) {
+        int now = extra_digit;
+        extra_digit = 0;
+        if (digits.size() - precision >= i + 1)
+            now += digits[digits.size() - precision - i - 1];
 
-        for (int i = 0; i < max_digit; i++) {
-            int now = extra_digit;
-            extra_digit = 0;
-            if (digits.size() - precision >= i + 1)
-                now += digits[digits.size() - precision - i - 1];
+        if (other.digits.size() - other.precision >= i + 1)
+            now += other.digits[other.digits.size() - other.precision - i - 1];
 
-            if (other.digits.size() - other.precision >= i + 1)
-                now += other.digits[other.digits.size() - other.precision - i - 1];
-
-            if (now > 1) {
-                extra_digit = 1;
-                now -= 2;
-            }
-            result.digits[result.digits.size() - result.precision - i - 1] = now;
+        if (now > 1) {
+            extra_digit = 1;
+            now -= 2;
         }
-        if (extra_digit)
-            result.digits[0] = 1;
+        result.digits[result.digits.size() - result.precision - i - 1] = now;
+    }
+    if (extra_digit)
+        result.digits[0] = 1;
 
-        result.deleteZeros();
-        return result;
+    result.deleteZeros();
+    return result;
 }
 
 LongNumber LongNumber::operator-(const LongNumber& other) const { 
@@ -159,20 +159,16 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
     result.digits.resize(max_precision + max_digit + 1);
     result.precision = max_precision;
     int extra_digit = 0;
+    int last_1 = digits.size() - 1;
+    int last_2 = other.digits.size() - 1;
         for (int i = 0; i < max_precision; i++) {
             int now = extra_digit;
             extra_digit = 0;
-            if (precision >= i)
-                if (diff >= 0)
-                    now += digits[digits.size() - i - 1];
-                else
-                    now += digits[digits.size() - i - 1 - diff];
-
-            if (other.precision >= i)
-                if (diff <= 0)
-                    now -= other.digits[other.digits.size() - i - 1];
-                else
-                    now -= other.digits[other.digits.size() - i - 1 + diff];
+            if (max_precision - precision <= i)
+                now += digits[last_1--];
+                
+            if (max_precision - other.precision <= i)
+                now -= other.digits[last_2--];
             
             if (now < 0) {
                 extra_digit = -1;
@@ -190,15 +186,12 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
             if (other.digits.size() - other.precision >= i + 1)
                 now -= other.digits[other.digits.size() - other.precision - i - 1];
 
-            //std::cout << now << " ";
             if (now < 0) {
                 extra_digit = -1;
                 now += 2;
             }
-            //std::cout << now << "\n";
             result.digits[result.digits.size() - result.precision - i - 1] = now;
         }
-        
         
         result.isNegative = isNegative;
         result.deleteZeros();
@@ -206,7 +199,7 @@ LongNumber LongNumber::operator-(const LongNumber& other) const {
 }
 
 LongNumber& LongNumber::operator-=(const LongNumber& other) {
-    *this = *this - other;
+   return *this = *this - other;
 }
 
 void LongNumber::deleteZeros() {
@@ -391,14 +384,44 @@ std::string LongNumber::toString() const {
     if (isNegative) {
         result += '-';
     }
-    for (int digit : digits) {
-        result += std::to_string(digit);
+    u_int64_t bin = (1ll << (digits.size() - precision - 1));
+    u_int64_t res_int = 0;
+    for (int i = 0; i < digits.size() - precision; i++) {
+        if (digits[i]) res_int += bin; 
+        bin /= 2;
     }
+    long double res_float = 0;
+    long double bin_float = 0.5;
+    for (int i = digits.size() - precision; i < digits.size(); i++) {
+        if (digits[i]) res_float += bin_float; 
+        bin_float /= 2;
+    }
+    if (res_float)
+        result += std::to_string(res_int + res_float);
+    else 
+        result += std::to_string(res_int);
     return result;
 }
 
+LongNumber makePI(int acur){
+    LongNumber pi(0);
+    LongNumber multiplier(1), denominator(16);
+    LongNumber a1(4), b1(2), c1(1), d1(1);
+    LongNumber den1(1), den2(4), den3(5), den4(6);
+    LongNumber eight(8);
+
+    for (int i = 0; i < acur; i++) {
+        pi = pi + multiplier * (a1 / den1 - b1 / den2 + c1 / den3 + d1 / den4);
+
+        multiplier = multiplier / denominator;
+        den1 = den1 + eight;
+        den2 = den2 + eight;
+        den3 = den3 + eight;
+        den4 = den4 + eight;
+    }
+    return pi;
+}
 void LongNumber::addDigit(int digit) {
     digits.push_back(digit);
 }
-
 
