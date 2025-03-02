d41e95f9b2b7cfb6a4eb9bb88377495a2e91fac5
tgenchik <gteplakov5@gmail.com>
2025-03-02 19:00:45 +0300

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <chrono>
 
-LongNumber makePI(int acur){
+LongNumber makePI(int acur) {
     LongNumber pi(0);
     LongNumber multiplier(1), denominator(16);
     LongNumber a1(4), b1(2), c1(1), d1(1);
@@ -21,40 +21,33 @@ LongNumber makePI(int acur){
     return pi;
 }
 
-int main(int argc, char *argv[])
-{
-    int decimal_precision{100};
+int main(int argc, char *argv[]) {
+    int dec_precision{100};
 
-    if(argc == 2)
-    {
-        try
-        {
-            decimal_precision = std::stoi(argv[1]);
+    if(argc == 2) {
+        try {
+            dec_precision = std::stoi(argv[1]);
         }
-        catch(...)
-        {
+        catch(...) {
             std::cerr << "Conversion error\n";
             return EXIT_FAILURE;
         }
     }
 
-    if(decimal_precision < 0)
-    {
+    if(dec_precision < 0) {
         std::cerr << "Negative precision\n";
         return EXIT_FAILURE;
     }
 
 
     auto start{ std::chrono::high_resolution_clock::now() };
-
-    LongNumber pi = makePI(decimal_precision);
-
+    LongNumber pi = makePI(dec_precision);
     auto end{ std::chrono::high_resolution_clock::now() };
     auto duration { std::chrono::duration_cast<std::chrono::milliseconds>(end - start) };
     
-    std::cout << "The first " << decimal_precision << " digits after the decimal point of pi are:\n";
-    std::cout << pi.toString(decimal_precision) << "\n\n";
-    std::cout << "Calculation time: " << duration << '\n';
+    std::cout << "First " << dec_precision << " digits after the decimal point of pi are:\n";
+    std::cout << pi.toString(dec_precision) << "\n\n";
+    std::cout << "Computed in: " << duration << '\n';
 
     return 0;
 }
